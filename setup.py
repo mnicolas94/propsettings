a2971105bae02e6a91f9854fcd61395661d868d9
miguel <miguelcok27@gmail.com>
2021-07-01 14:59:01 -0400

    fix: push new tag only once (after publishing)

--- setup.py
+++ setup.py
@@ -1,49 +1,13 @@
 import setuptools
-import subprocess
 
-
-def get_last_version():
-    bash_command = "git describe --tags --abbrev=0 --match v[0-9]*"
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    outputstr = str(output)
-    outputstr = outputstr[3:-3]  # "b'vXX.XX.XX\\n'" becomes "XX.XX.XX"
-    outputstr = outputstr.replace('-', '.')
-    major, minor, patch, *_ = outputstr.split('.')
-    version = f'{major}.{minor}.{patch}'
-    return version
-
-
-def bump_patch(version: str):
-    major, minor, patch = version.split('.')
-    bumped = str(int(patch) + 1)
-    return f'{major}.{minor}.{bumped}'
-
-
-def push_new_version_as_tag(version):
-    bash_command = f"git tag v{version}"
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    bash_command = f"git push origin v{version}"
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-
-
-def echo(string):
-    bash_command = f"echo {string}"
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-
+import setup_versioning
 
 if __name__ == '__main__':
     with open("README.md", "r") as fh:
         long_description = fh.read()
 
-    last_version = get_last_version()
-    version = bump_patch(last_version)
-
-    echo(f'last version: {last_version}')
-    echo(f'new version: {version}')
+    last_version = setup_versioning.get_last_version()
+    version = setup_versioning.bump_patch(last_version)
 
     setuptools.setup(
         name="propsettings",
@@ -64,6 +28,3 @@ if __name__ == '__main__':
         ],
         python_requires='>=3.6',
     )
-
-    echo('Pushing new tag ...')
-    push_new_version_as_tag(version)
